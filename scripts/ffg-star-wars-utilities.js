39d60e1af2346969c91693a987e7b696d6c5b12b
TeddyBears <tdonnard@free.fr>
2024-05-16 14:56:04 +0200

    Add buttons to apply critical or damage after a weapon roll

--- scripts/ffg-star-wars-utilities.js
+++ scripts/ffg-star-wars-utilities.js
@@ -1,17 +1,21 @@
-import { init } from "./dice_helper.js";
+import { init as dice_init} from "./dice_helper.js";
+import { init as damage_init, damage_helper } from "./damage_helper.js";
 
 Hooks.on('init', () => {
     if (typeof Babele !== 'undefined') {
-
         Babele.get().register({
             module: 'ffg-star-wars-utilities',
             lang: 'fr',
             dir: 'lang/compendium'
         });
     }
+    damage_init();
+    damage_helper();
 });
 
 Hooks.once("ready", () => {
-    init();
+    dice_init();
 });
+
+
 
