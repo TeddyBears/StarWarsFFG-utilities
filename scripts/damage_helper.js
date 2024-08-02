5213de1bd0cd460c4eee836ff4efa7ddf455f5d1
TeddyBears <tdonnard@free.fr>
2024-08-02 21:17:49 +0200

    override FFGroll template to add buttons to apply injury and critical. Player can use critical button now to roll the table

--- scripts/damage_helper.js
+++ scripts/damage_helper.js
@@ -1,3 +1,5 @@
+import { RollFFG } from "../../../systems/starwarsffg/modules/dice/roll.js";
+
 export function init() {
     game.settings.register("ffg-star-wars-utilities", "damage-helper-active", {
         name: game.i18n.localize("ffg-star-wars-utilities.damage-helper.active.label"),
@@ -21,8 +23,10 @@ export function init() {
             location.reload();
         }
     });
-}
 
+    //if it is a result from a weapon test, add chat button to apply damage and critical
+    RollFFG.CHAT_TEMPLATE = "modules/ffg-star-wars-utilities/templates/dice/roll-ffg.html";
+}
 
 /** Check chat message. if it is a result from a weapon test, add chat button to apply damage and critical
  * 
@@ -32,59 +36,44 @@ export async function damage_helper() {
     if (!game.settings.get("ffg-star-wars-utilities", "damage-helper-active")) {
         return;
     }
-    Hooks.on("renderChatMessage", (messageData, meta_data, id) => {
-        if (!game.user.isGM) return;
-        if (messageData.isRoll) {
-            let roll = messageData.rolls[0];
-            let successNumber = roll.ffg ? parseInt(roll.ffg.success) : 0;
+    Hooks.on("renderChatMessage", (app, html, messageData) => {
+        html.on("click", ".ffg-utilities-damage", async function () {
+            await apply_damage(app.rolls[0]);
+        });
+        html.on("click", ".ffg-utilities-critical", async function () {
+            await apply_critical(app, true);
+        });
 
-            if (roll.data.type === "weapon"
-                && successNumber > 0) {
-                let weapon = roll.data;
-                let criticalHit = (weapon.system.crit?.adjusted) ? weapon.system.crit.adjusted : weapon.system.crit.value;
-                let tr = roll.ffg.triumph;
-                let ad = roll.ffg.advantage;
-                let actions = {}, data = {};
-                let tokenTargeted = canvas.tokens.get(messageData.user.targets.ids[0]);
-
-                actions.damage = "apply_damage"; //it's a success, so apply damage by default
-                data.weapon = weapon
-                data.isGM = game.user.isGM;
-                data.message = messageData;
-                data.target = tokenTargeted ? tokenTargeted : null;
-
-                if (tr > 0 || ad >= criticalHit) {
-                    actions.critical = "apply_critical";
-                    data.isCritical = true;
-                }
-                appendChatMessage(messageData._id, "buttons", data, actions);
-            }
-        }
     });
 }
 
-let Handler = {};
-Handler.apply_damage = async function (data) {
-    if (canvas.tokens.controlled.length === 0) {
-        ui.notifications.info(game.i18n.localize('ffg-star-wars-utilities.token.noSelected'));
+
+async function apply_damage(data) {
+    if (!game.user.isGM) {
+        ui.notifications.info(game.i18n.localize('ffg-star-wars-utilities.user.notGM'));
         return;
     }
-    let target = canvas.tokens.controlled[0];
-    let weapon = data.weapon
+    if (game.user.targets.ids.length === 0) {
+        ui.notifications.info(game.i18n.localize('ffg-star-wars-utilities.token.notarget'));
+        return;
+    }
+    console.log(game.user)
+    let target = canvas.tokens.get(game.user.targets.ids[0]);
+    let weapon = data.data
     let chatContent = "";
 
     if (target) {
         let soak = parseInt(target.actor.system.stats.soak.value);
         let oldWounds = Number.isInteger(parseInt(target.actor.system.stats.wounds.value)) ? parseInt(target.actor.system.stats.wounds.value) : 0;
         let oldStrain = Number.isInteger(parseInt(target.actor.system.stats.strain.value)) ? parseInt(target.actor.system.stats.strain.value) : 0;
-        let pierce = 0, breach = 0, haveCortosis = false;
 
+        let pierce = 0, breach = 0, haveCortosis = false;
         let pierceList = await weapon.system.itemmodifier.filter(w => w.name.toLowerCase().startsWith("pierce"));
         let breachList = await weapon.system.itemmodifier.filter(w => w.name.toLowerCase().startsWith("breach"));
         let isStrain = await weapon.system.itemmodifier.filter(w => w.name.toLowerCase().startsWith("stun damage"));
+
         let armor = await target.actor.items.filter(item => item.type === "armour");
         armor.forEach(function (currentArmor) {
-
             if (currentArmor.system.equippable.equipped) {
                 let cortosisMod = currentArmor.system.itemmodifier.filter(item => item.name.toLowerCase().startsWith("cortosis"));
                 if (cortosisMod.length > 0) {
@@ -105,7 +94,7 @@ Handler.apply_damage = async function (data) {
         let leftoverSoak = (soak - (pierce + breach));
         leftoverSoak = (leftoverSoak < 0) ? 0 : leftoverSoak;
         let baseDamage = (weapon.system.damage?.adjusted) ? weapon.system.damage.adjusted : weapon.system.damage.value;
-        let extraDamage = parseInt(data.message.rolls[0].ffg.success);
+        let extraDamage = parseInt(data.ffg.success);
         let totalDamage = parseInt(baseDamage + extraDamage);
         let damageTaken = (parseInt(totalDamage - leftoverSoak) < 0) ? 0 : parseInt(totalDamage - leftoverSoak);
         let damageType = "", damageValue = 0;
@@ -146,29 +135,29 @@ Handler.apply_damage = async function (data) {
 }
 
 
-Handler.apply_critical = async function (data) {
-    if (canvas.tokens.controlled.length === 0) {
-        ui.notifications.info(game.i18n.localize('ffg-star-wars-utilities.token.noSelected'));
+async function apply_critical(data) {
+    if (game.user.targets.ids.length === 0) {
+        ui.notifications.info(game.i18n.localize('ffg-star-wars-utilities.token.notarget'));
         return;
     }
-    let token = canvas.tokens.controlled[0];
+    let target = canvas.tokens.get(game.user.targets.ids[0]);
 
-    if (token.actor.type === "minion") {
+    if (target.actor.type === "minion") {
         // minions don't take critical injuries. Just reduce the quantity by 1
 
-        let unit_wounds = token.actor.system.unit_wounds.value
-        let currentWounds = token.actor.system.stats.wounds.value
-        let maxWound = token.actor.system.quantity.max * unit_wounds + 1
+        let unit_wounds = target.actor.system.unit_wounds.value
+        let currentWounds = target.actor.system.stats.wounds.value
+        let maxWound = target.actor.system.quantity.max * unit_wounds + 1
         let newUnit_wound = currentWounds + unit_wounds + 1
 
-        await token.actor.update({ ['system.stats.wounds.value']: Math.min(maxWound, parseInt(newUnit_wound)) });
+        await target.actor.update({ ['system.stats.wounds.value']: Math.min(maxWound, parseInt(newUnit_wound)) });
     } else {
         const tables = game.tables.map(table => {
             if (table.name.includes("Critical")) {
-                if (token != null && ((token.actor.type === "vehicle" && table.name === "Critical Damage")
-                    || (token.actor.type === "character" && table.name === "Critical Injuries")
-                    || (token.actor.type === "nemesis" && table.name === "Critical Injuries")
-                    || (token.actor.type === "rival" && table.name === "Critical Injuries")
+                if (target != null && ((target.actor.type === "vehicle" && table.name === "Critical Damage")
+                    || (target.actor.type === "character" && table.name === "Critical Injuries")
+                    || (target.actor.type === "nemesis" && table.name === "Critical Injuries")
+                    || (target.actor.type === "rival" && table.name === "Critical Injuries")
 
                 )) {
                     return `<option value="${table._id}" selected>${table.name}</option>`;
@@ -182,16 +171,16 @@ Handler.apply_critical = async function (data) {
         let modifier = 0, durableValue = 0, durableRank = 0;
 
         //Count the number of injuries the token already has
-        modifier = token.actor.items.filter(item => item.type === "criticalinjury" || item.type === "criticaldamage").length * 10;
+        modifier = target.actor.items.filter(item => item.type === "criticalinjury" || item.type === "criticaldamage").length * 10;
 
         //Check if the attacker has lethal blows talents
-        let attackerToken = await game.actors.get(data.message.speaker.actor);
+        let attackerToken = await game.actors.get(data.speaker.actor);
         let lethalBlowsTalent = attackerToken.talentList.filter(item => item.name.toLowerCase().includes(game.i18n.localize("ffg-star-wars-utilities.damage-helper.items.lethalBlows")));
         if (lethalBlowsTalent.length > 0) {
             modifier += lethalBlowsTalent[0].rank * 10;
         }
-        //check to see if the token has the Durable talent
-        let durableTalent = token.actor.items.filter(item => item.name.toLowerCase() === "durable");
+        //check to see if the target has the Durable talent
+        let durableTalent = target.actor.items.filter(item => item.name.toLowerCase() === "durable");
         //If the talent is found multiply it by 10 for the roll
         if (durableTalent.length > 0) {
             durableRank = durableTalent[0].system.ranks.current
@@ -223,22 +212,27 @@ Handler.apply_critical = async function (data) {
                             displayChat: true
                         });
                         //If we have an actor selected try to add the injury
-                        if (token) {
+                        if (target) {
                             //Table roles are async so wait for it to return
                             tableResult.then(function (value) {
                                 //Ignore if we didn't draw a result
                                 if (value.results.length <= 0) {
                                     return;
                                 }
-
-                                var firstResult = value.results[0];
-                                var item = game.items.get(firstResult.documentId);
+                                let msgContent = ""
+                                let firstResult = value.results[0];
+                                let item = game.items.get(firstResult.documentId);
                                 if (item != null) {
                                     //Add injury to the selected chracter
-                                    token.actor.createEmbeddedDocuments("Item", [item.toObject()]);
+                                    if (game.user.isGM) {
+                                        target.actor.createEmbeddedDocuments("Item", [item.toObject()]);
+                                    } else {
+                                        msgContent = game.i18n.localize('ffg-star-wars-utilities.damage-helper.apply_critical.notApply')
+                                    }
+
                                     ChatMessage.create({
-                                        speaker: { alias: token.actor.name, token: token.actor.id },
-                                        content: item.system.description
+                                        speaker: { alias: target.actor.name, token: target.actor.id },
+                                        content: msgContent + item.system.description
                                     })
                                 }
                             });
@@ -256,30 +250,4 @@ Handler.apply_critical = async function (data) {
         });
         d.render(true);
     }
-}
-
-/* display action buttons
- *
- */
-async function _appendToDamageRolls(message) {
-    const div = document.createElement("DIV");
-    let weapon = message.rolls[0].data;
-    let roll = message.rolls[0];
-
-    div.innerHTML = await renderTemplate("modules/ffg-star-wars-utilities/templates/buttons.hbs", roll);
-    div.querySelectorAll("[data-action]").forEach(n => {
-        if (parseInt(roll.ffg.success) > 0) n.addEventListener("click", _quickApply.bind());
-    });
-}
-
-async function appendChatMessage(messageId, templateName, data, bindActions) {
-    let div = document.createElement("DIV");
-    div.innerHTML = await renderTemplate("modules/ffg-star-wars-utilities/templates/" + templateName + ".hbs", data);
-
-    div.querySelectorAll("[data-action]").forEach(n => {
-        const action = n.dataset.action;
-        if (bindActions[action]) n.addEventListener("click", Handler[bindActions[action]].bind(this, data));
-    });
-
-    $("[data-message-id=" + messageId + "] .message-content").append(div.firstElementChild);
 }
